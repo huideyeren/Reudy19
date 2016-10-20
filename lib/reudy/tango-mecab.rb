213b28a9dfc1d3f0ca616b842e1c95f836f1d563
Iosif Huideyeren Takakura <takakura.yusuke@gmail.com>
2016-10-20 19:47:31 +0900

    mecabの読み取り方法を変更

--- lib/reudy/tango-mecab.rb
+++ lib/reudy/tango-mecab.rb
@@ -1,16 +1,17 @@
 #encoding:utf-8
 #Copyright (C) 2011 Glass_saga <glass.saga@gmail.com>
 
-require 'MeCab'
+require 'mecab'
 
 class WordExtractor
   # WordExtractor(単語候補リストを保持する長さ,単語追加時のコールバック)
-  
-  POS_ID = [38,41,42,43,44,45,46,47] #単語として扱う品詞リスト  
+
+  POS_ID = [38,41,42,43,44,45,46,47] #単語として扱う品詞リスト
 
   def initialize(candlistlength=7,onaddword=nil)
     @onAddWord = onaddword
-    @m = MeCab::Tagger.new
+    userdic_path = "./mecab-dict/custom.dic"
+    @m = MeCab::Tagger.new("-u #{userdic_path}")
   end
 
   # 文中で使われている単語を取得
